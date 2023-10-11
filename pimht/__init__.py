e2f778e6fb1ac92a4b19eafb7147f0cd7da209ab
Pilate <noptical@gmail.com>
2023-10-10 17:32:16 -0700

    add url
    add __all__ to init

--- pimht/__init__.py
+++ pimht/__init__.py
@@ -1,1 +1,10 @@
-from .pimht import from_bytes, from_string, from_filename, from_fileobj
+from .pimht import (
+    from_bytes,
+    from_string,
+    from_filename,
+    from_fileobj,
+    MHTML,
+    MHTMLPart,
+)
+
+__all__ = [from_bytes, from_string, from_filename, from_fileobj, MHTML, MHTMLPart]

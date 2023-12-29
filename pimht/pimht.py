8ac67acbaf252431bb109e6afd2d6f6d6b6de738
Pilate <noptical@gmail.com>
2023-12-28 18:40:00 -0800

    add docstrings

--- pimht/pimht.py
+++ pimht/pimht.py
@@ -17,7 +17,7 @@ __version__ = version("pimht")
 
 class FasterParser(email.parser.Parser):
     """
-    Built-in 'Parser' will only process 8kb at a time
+    Replacement for email.parser.Parser that removes 8kb chunking.
     https://stackoverflow.com/questions/3543118/python-email-message-from-string-performance-with-large-data-in-email-body
     """
 
@@ -31,39 +31,58 @@ class FasterParser(email.parser.Parser):
 
 
 class FasterBytesParser(email.parser.BytesParser):
+    """Replacement for email.parser.BytesParser that removes 8kb chunking."""
+
     def __init__(self, *args, **kwargs):
         self.parser = FasterParser(*args, **kwargs)
 
 
 class MHTMLPart:
+    """Part of an MHTML archive."""
+
     def __init__(self, message: email.message.Message):
         self.message = message
 
         self.headers = dict(message)
+        """Headers for the MHTML part."""
 
-        self.content_type = self.message.get_content_type()
-        self.is_text = self.content_type.startswith("text/")
+    @cached_property
+    def content_type(self) -> str:
+        """Content-type of the MHTML part."""
+        return self.message.get_content_type()
+
+    @cached_property
+    def is_text(self) -> bool:
+        """Whether or not the parts content-type begins with 'text'."""
+        return self.content_type.startswith("text/")
 
     @cached_property
     def raw(self) -> bytes:
+        """The raw (bytes) content of the MHTML part."""
         return self.message.get_payload(decode=True) or b""
 
-    @property
-    def charset(self) -> dict:
-        return chardet.detect(self.raw)
-
-    @property
+    @cached_property
     def text(self) -> str:
+        """
+        String representation of the MHTML part.
+        Raises TypeError if the part cannot be decoded.
+        """
         if not self.is_text:
             raise TypeError("MHTMLPart is not text")
 
-        return self.raw.decode(self.charset["encoding"] or "utf-8", "ignore")
+        charset = chardet.detect(self.raw)
+        return self.raw.decode(charset["encoding"] or "utf-8", "ignore")
 
     def __str__(self) -> str:
         return f"<{self.__class__.__name__} headers={self.headers}>"
 
 
 class MHTML:
+    """
+    MHTML archive.
+    Objects are iterable to allow processing/inspection of archive contents.
+    """
+
     def __init__(self, message: email.message.Message):
         self.message = message
 
@@ -76,16 +95,19 @@ class MHTML:
 
 
 def from_bytes(mhtml_bytes: bytes) -> MHTML:
+    """Parse bytes as an MHTML object."""
     message = FasterBytesParser().parsebytes(mhtml_bytes)
     return MHTML(message)
 
 
 def from_string(mhtml_str: str) -> MHTML:
+    """Parse a string as an MHTML object."""
     message = FasterParser().parsestr(mhtml_str)
     return MHTML(message)
 
 
 def from_fileobj(fileobj: typing.IO) -> MHTML:
+    """Parse a file object as an MHTML object."""
     if "b" in fileobj.mode:
         message = FasterBytesParser().parse(fileobj)
     else:
@@ -94,6 +116,7 @@ def from_fileobj(fileobj: typing.IO) -> MHTML:
 
 
 def from_filename(filename: str) -> MHTML:
+    """Parse the contents of a file path as an MHTML object."""
     with open(filename, "rb") as fileobj:
         return from_fileobj(fileobj)
 
